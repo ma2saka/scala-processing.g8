fdeb7fa14bc0da61795f1f2db4c2d5ada4464681
ma2saka <takashi.matsusaka@me.com>
2017-02-21 19:05:50 +0900

    初期コードの修正

--- src/main/g8/src/main/scala/Applet.scala
+++ src/main/g8/src/main/scala/Applet.scala
@@ -4,10 +4,11 @@ import processing.core.PApplet
 import processing.core.PConstants._
 
 class Applet extends PApplet{
+  lazy val BG_COLOR = { color(0,0,100) }
 
   override def settings(): Unit = {
 
-    size(200, 200, JAVA2D)
+    size(320, 240, JAVA2D)
 
   }
 
@@ -15,17 +16,23 @@ class Applet extends PApplet{
   override def setup(): Unit = {
 
     surface.setTitle("$title$")
+    smooth()
+    frameRate(30)
 
   }
 
 
   override def draw(): Unit ={
 
-    background(0)
-    textAlign(LEFT, TOP)
+    background(BG_COLOR)
 
-    text("%d, %d".format(mouseX,mouseY),0,0)
+    textAlign(LEFT, TOP)
+    text(s"$"$"$mouseX, $"$"$mouseY",0 ,0)
+
+    textAlign(CENTER, CENTER)
+    text("$title$", width/2, height/2)
 
   }
 }
+
 
